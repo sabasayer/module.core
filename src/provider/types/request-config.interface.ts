153b28056ba72529ec81d700056a38554c2b299a
salih <salihbakisayer@hotmail.com.tr>
2021-01-16 21:01:42 +0300

    refactor(request-config)

--- src/provider/types/request-config.interface.ts
+++ src/provider/types/request-config.interface.ts
@@ -1,9 +1,9 @@
-export type IRequestConfig<Request = undefined, Response = undefined> = {
+export type IRequestConfig<TRequest = undefined, TResponse = undefined> = {
   url: string;
   cacheKey?: string;
 };
 
 export type ICachableRequestConfig<
-  Request = undefined,
-  Response = undefined
-> = IRequestConfig<Request, Response> & { cacheKey: string };
+  TRequest = undefined,
+  TResponse = undefined
+> = IRequestConfig<TRequest, TResponse> & { cacheKey: string };
