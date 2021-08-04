3a9ce984befafa7ac7ba666c1c52b1f12c87815a
salih <salihbakisayer@hotmail.com.tr>
2021-08-04 17:15:46 +0300

    version bump

--- src/localization/mocks.ts
+++ src/localization/mocks.ts
@@ -1,7 +1,8 @@
 import { defaultLocalization } from "./default-localization";
 import { globalModule } from "../global-module/global-module";
+import type { Translations } from "./types";
 
-export const mockTranslations = (translations: Record<string, string>) => {
+export const mockTranslations = (translations: Translations) => {
   defaultLocalization.setLang("en-us");
   defaultLocalization.setTranslations({
     "en-us": translations,
